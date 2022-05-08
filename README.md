7fdf52c2ffebe75925c1addd0b8f9518830319b8
Administrator <admin@liucl.cn>
2022-05-08 13:50:17 +0800

    init

--- README.md
+++ README.md
@@ -1,31 +1,19 @@
-# ts-dev-template
-ts 开发基础配置环境
-## Notice
-这是一个模板仓库，可用于初始化Ts开发时的一些推荐配置
-## Usage
-1.调用模板创建仓库
+# oicq2-cq-enable
+让oicq2.x支持cq码
+使用方法：
+1.安装依赖
 ```shell
-点击仓库首页右边绿色按钮(Use This Template)或访问https://github.com/lc-cn/ts-dev-template/generate，根据github提示创建仓库
-```
-2.拉取你刚刚创建的仓库(`<>`表示括号内描述的内容为必填)
-```shell
-git clone <你刚刚创建的仓库地址>
-```
-2.安装依赖
-```shell
-npm install
+npm install oicq2-cq-enable
 # 或 npm i
 ```
-3.运行开发环境(ts-node-dev运行ts代码)
-```shell
-npm run dev
+2.在你的项目入口文件最开头导入依赖
+js
+```javascript
+require('oicq2-cq-enable')
 ```
-4.将ts编译成js(编译代码成普通node可执行的js代码)
-```shell
-npm run build
+ts
+```typescript
+import 'oicq2-cq-enable'
 ```
-5.执行编译后的js(通常生产环境调用)
-```shell
-npm start
-# 或 npm run start
-```
+然后，你的Client的message相关事件的event将会携带cqCode参数，
+event的reply和一切sendMsg将支持直接发送cq码
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-import {sayHello} from "@/hello";
-console.log(sayHello('world'))
+import {Client,Group,Discuss,Friend} from "oicq";
+import {fromCqcode, toCqcode} from "@/utils";
+declare module 'oicq'{
+    class Message{
+        cqCode:string
+    }
+}
+function bindFromCqcode(target){
+    const oldSendMsg=target.prototype.sendMsg
+    target.prototype.sendMsg=function (content,quote){
+        content=[].concat(content).map(msg=>typeof msg==='string'?fromCqcode(msg):msg).flat()
+        return oldSendMsg.apply(this,[content,quote])
+    }
+}
+bindFromCqcode(Group)
+bindFromCqcode(Discuss)
+bindFromCqcode(Friend)
+const oldEmit=Client.prototype.emit
+Client.prototype.emit=function (event,data,...args){
+    if(typeof event!=='string' || !event.startsWith('message'))return oldEmit.apply(this,[event,data,...args])
+    if(typeof data==='object' && ('message' in data) && Array.isArray(data.message)){
+        data.cqCode=toCqcode(data)
+    }
+    return oldEmit.apply(this,[event,data,...args])
+}
